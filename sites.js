4c6b3cda368a489b8b6ad21317c16b2c3a76d539
RafaelWolde <rafaelwolde2030@gmail.com>
2023-03-19 16:16:20 +0300

    New site deployed

--- sites.js
+++ sites.js
@@ -4,8 +4,11 @@ const sitesList = [
     { title: 'Frontend Mentor | Notifications page', relativePath: './sites/plain/notifications-page-main/index.html', description: 'Frontend Mentor challenge for newbies   a single page for notifications' },
     { title: 'Frontend Mentor | Product preview card component', relativePath: './sites/plain/product-preview-card-component-main/index.html', description: 'This is a FrontendMentor challenge Which I build using React.' },
     { title: 'Frontend Mentor | QR code component', relativePath: './sites/plain/qr-code-component-main/index.html', description: 'This is my very first challenge    from frontendMentor which is a QRCode scanner card.' },
+    { title: 'Frontend Mentor | Results summary component', relativePath: './sites/plain/results-summary-component-main/index.html', description: '' },
+    { title: 'Document', relativePath: './sites/plain/index.html', description: '' },
     { title: 'APP | Frontend Mentor intro section with dropdown', relativePath: './sites/react/intro-section-with-dropdown/static/index.html', description: 'This is a frontend mentor challenge      it is built using React.js' },
     { title: 'First Deployed React App', relativePath: './sites/react/index.html', description: 'This Is An App Which will be deployed soon.' },
+    { title: 'Document', relativePath: './sites/index.html', description: '' },
     { title: 'RAFAEL WOLDIE', relativePath: './index.html', description: 'This Is MY Github Pages Home page' },
 ]
 
